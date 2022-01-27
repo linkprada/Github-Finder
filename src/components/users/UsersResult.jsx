b165a6f3bc60dd44dba56eefa8dacb57ce1decc8
linkprada <haouaouchi.said@gmail.com>
2022-01-27 21:34:00 +0100

    Add UserItem Component

--- src/components/users/UsersResult.jsx
+++ src/components/users/UsersResult.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import UsersContext from "../../context/UsersContext";
 import Spinner from "../layouts/Spinner";
+import UserItem from "./UserItem";
 
 function UsersResult() {
     const {users, loading} = useContext(UsersContext);
@@ -9,7 +10,7 @@ function UsersResult() {
         return <Spinner></Spinner>
     }
 
-    var usersList = users.map((user) => <h3 key={user.id}>{user.login}</h3>);
+    var usersList = users.map((user) => <UserItem key={user.id} user={user}></UserItem>);
     
     return <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {usersList}
