1d92283502c8d1e079bdad48eee00eb090b5bb52
linkprada <haouaouchi.said@gmail.com>
2022-01-28 21:11:44 +0100

    Add UserSearch Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import UserSearch from "../components/users/UserSearch";
 import UsersResult from "../components/users/UsersResult";
 
 function Home() {
-    return <div>
-        <UsersResult></UsersResult>
-    </div>;
+    return (
+        <div>
+            <UserSearch></UserSearch>
+            <UsersResult></UsersResult>
+        </div>
+    );
 }
 
 export default Home;
