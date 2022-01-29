b0444064cef5221fb59aad19b48451d7b2da880e
linkprada <haouaouchi.said@gmail.com>
2022-01-29 18:11:00 +0100

    Add AlertContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layouts/Footer";
 import Navbar from "./components/layouts/Navbar";
+import { AlertContextProvider } from "./context/AlertContext";
 import { GithubContextProvider } from "./context/GithubContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -9,20 +10,22 @@ import NotFound from "./pages/NotFound";
 function App() {
     return (
         <GithubContextProvider>
-            <Router>
-                <div className="flex flex-col justify-between h-screen">
-                    <Navbar></Navbar>
-                    <main className="container mx-auto px-3 pb-12">
-                        <Routes>
-                            <Route exact path="/" element={<Home></Home>}></Route>
-                            <Route path="/about" element={<About></About>}></Route>
-                            <Route path="/notfound" element={<NotFound></NotFound>}></Route>
-                            <Route path="/*" element={<NotFound></NotFound>}></Route>
-                        </Routes>
-                    </main>
-                    <Footer></Footer>
-                </div>
-            </Router>
+            <AlertContextProvider>
+                <Router>
+                    <div className="flex flex-col justify-between h-screen">
+                        <Navbar></Navbar>
+                        <main className="container mx-auto px-3 pb-12">
+                            <Routes>
+                                <Route exact path="/" element={<Home></Home>}></Route>
+                                <Route path="/about" element={<About></About>}></Route>
+                                <Route path="/notfound" element={<NotFound></NotFound>}></Route>
+                                <Route path="/*" element={<NotFound></NotFound>}></Route>
+                            </Routes>
+                        </main>
+                        <Footer></Footer>
+                    </div>
+                </Router>
+            </AlertContextProvider>
         </GithubContextProvider>
     );
 }
